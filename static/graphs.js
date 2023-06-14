ca554e60dbd2da1223a627aa94993b5d3ca1eff5
meganflinders <meganflinders2@gmail.com>
2023-06-14 09:01:52 -0400

    working on line graphs

--- static/graphs.js
+++ static/graphs.js
@@ -1,21 +1,151 @@
 const getFastFood = () => {
   fetch('http://localhost:8080/fastfood-nutrition')
     .then(response => response.json())
-    .then(fastFoodData => {
-      // Do something with the fastFoodData
+    .then(fastFoodData => {      
+      // Extract the nutrients and their values from the data
+      
+  }) 
+}
+// Function to populate the item dropdown based on the selected restaurant
+// async function populateItemDropdown(restaurant) {
+//   const itemSelect = document.getElementById("item-select");
+//   itemSelect.innerHTML = ""; // Clear existing options
 
-      const someHtmlElement = document.getElementById('fast-food-scatter-plot');
-      const spanElement = document.createElement('span');
-      spanElement.textContent = 'Hi Hello';
-      someHtmlElement.appendChild(spanElement);
+//   // Get fast food data
+//   const fastFoodData = await getFastFood();
 
-      console.log(fastFoodData);
-    })
-    .catch(error => {
-      // Handle any errors that occurred during the fetch request
-      console.error('Error:', error);
-    });
-};
+//   // Filter the data based on the selected restaurant
+//   const filteredData = fastFoodData.filter(obj => obj.restaurant === restaurant);
+
+//   // Get unique items for the selected restaurant
+//   const items = [...new Set(filteredData.map(obj => obj.item))];
+
+//   // Create options for each item
+//   items.forEach(item => {
+//     const option = document.createElement("option");
+//     option.value = item;
+//     option.textContent = item;
+//     itemSelect.appendChild(option);
+//   });
+// }
+
+
+// Event listener for the restaurant dropdown
+// function createLineGraph(restaurant, item, fastFoodData) {
+//   // Get the data for the selected restaurant and item
+//   const filteredData = fastFoodData.filter((obj) => obj.restaurant === restaurant && obj.item === item);
+  
+//   // Extract the nutrients and their values from the data
+//   const nutrients = ['cal_fat', 'total_fat', 'sat_fat', 'trans_fat', 'cholesterol', 'sodium', 'total_carb', 'fiber', 'sugar', 'protein'];
+//   const nutrientLabels = nutrients.map((nutrient) => nutrient.replace('_', ' '));
+//   const nutrientData = nutrients.map((nutrient) => filteredData[0][nutrient]);
+  
+
+//   // Create the line chart
+//   const ctx = document.getElementById('lineChart').getContext('2d');
+//   const lineChart = new Chart(ctx, {
+//     type: 'line',
+//     data: {
+//       labels: nutrientLabels,
+//       datasets: [
+//         {
+//           label: `${item} (${restaurant})`,
+//           data: nutrientData,
+//           fill: false,
+//           borderColor: 'rgb(75, 192, 192)',
+//           tension: 0.1
+//         }
+//       ]
+//     },
+//     options: {
+//       responsive: true,
+//       scales: {
+//         y: {
+//           beginAtZero: true,
+//           title: {
+//             display: true,
+//             text: 'Nutrient Content'
+//           }
+//         },
+//         x: {
+//           title: {
+//             display: true,
+//             text: 'Nutrients'
+//           }
+//         }
+//       }
+//     }
+//   });
+//   document.getElementById("restaurant-select").addEventListener("change", (event) => {
+//     const restaurant = event.target.value;
+//     populateItemDropdown(restaurant);
+//     createLineGraph(restaurant, item);
+// })
+// };
+function updateChart() {
+  const restaurantSelect = document.getElementById('restaurant-select');
+  const itemSelect = document.getElementById('item-select');
+  const selectedRestaurant = restaurantSelect.value;
+  const selectedItem = itemSelect.value;
+
+  const filteredData = fastFoodData.filter(
+    data => data.restaurant === selectedRestaurant && data.item === selectedItem
+  );
+
+  const nutrients = ['cal_fat', 'total_fat', 'sat_fat', 'trans_fat', 'cholesterol', 'sodium', 'total_carb', 'fiber', 'sugar', 'protein']; // Extract nutrient names
+  const nutrientValues = nutrients.map(nutrient => filteredData[0][nutrient]);
+
+  const ctx = document.getElementById('chart').getContext('2d');
+  const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: nutrients,
+      datasets: [
+        {
+          label: 'Nutrient Content',
+          data: nutrientValues,
+          fill: false,
+          borderColor: 'blue',
+          tension: 0.1
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        y: {
+          beginAtZero: true,
+          title: {
+            display: true,
+            text: 'Nutrient Content'
+          }
+        },
+        x: {
+          title: {
+            display: true,
+            text: 'Nutrient'
+          }
+        }
+      }
+    }
+  });
+}
+// Initial population of item dropdown based on the default selected restaurant
+// const initialRestaurant = document.getElementById("restaurant-select").value;
+// const initialItem = document.getElementById("item-select").value;
+// getFastFood()
+//   .then((fastFoodData) => {
+//     populateItemDropdown(initialRestaurant, fastFoodData);
+//     createLineGraph(initialRestaurant, initialItem, fastFoodData);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+const restaurantSelect = document.getElementById('restaurant-select');
+const itemSelect = document.getElementById('item-select');
+restaurantSelect.addEventListener('change', updateChart);
+itemSelect.addEventListener('change', updateChart);
 
 const getRestaurantAvgs = () => {
   fetch('http://localhost:8080/restaurant-averages')
